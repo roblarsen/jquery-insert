d15d5ef40e3379fc0d5ba0a3c5df2da0f91218c9
Rob Larsen <rob@drunkenfist.com>
2020-05-11 23:43:42 -0400

    this works!

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 "use strict";
-var jsdom = require("jsdom");
-var package_json = require("./package.json");
-var ssri = require("ssri");
-var fs = require("fs");
-var version = package_json.dependencies.jquery;
-var doc = jsdom.JSDOM.fromFile('../html5-boilerplate/dist/index.html', this.options).then(function (dom) {
-            var DOC = dom.window.document;
-            var script = DOC.createElement('script');
-            script.src = "https://code.jquery.com/jquery-" + version + ".min.js";
-            var hash = ssri.fromData(fs.readFileSync('./node_modules/jquery/dist/jquery.min.js'), { algorithms: ['sha256'] });
-           
-            script.setAttribute("integrity",hash.toString());
-          
-            var scriptBackup = DOC.createElement('script');
-            scriptBackup.innerHTML = `window.jQuery || document.write('<script src=\"js/vendor/jquery-${version}.min.js\"></script>')`;
-            var firstScript = DOC.querySelectorAll('body script')[0];
-            DOC.body.insertBefore(script, firstScript);
-            DOC.body.insertBefore(scriptBackup, firstScript);
-            fs.writeFile('./index.html', DOC.documentElement.outerHTML,
-                     function (error){
-            if (error) throw error;
-        });
-        });
+const package_json = require("./package.json");
+const ssri = require("ssri");
+const fs = require('fs');
+const version = package_json.dependencies.jquery;
+const hash = ssri.fromData(fs.readFileSync('./node_modules/jquery/dist/jquery.min.js'), { algorithms: ['sha256'] });
+const template = `<script src="https://code.jquery.com/${version}.min.js" integrity="${hash}" crossorigin="anonymous"></script>
+<script>window.jQuery || document.write('<script src="js/vendor/jquery-${version}.min.js"><\\/script>')</script>`;
+const indexHTML = fs.readFileSync('./index.html').toString().split("\n");
+const fourOhFourHTML = fs.readFileSync('./404.html').toString().split("\n");
+let index = 0;
+for (let i = 0; i < indexHTML.length; i++){
 
-       
+    if (indexHTML[i].indexOf("modernizr") > 0) {
+        index = i;
+    }
+}
+indexHTML.splice(index, 0, template);
+const indexText = indexHTML.join("\n");
+
+fs.writeFile('index.html', indexText, (error) => {
+if (error) {
+    return console.log(error);
+}  
+});
+
+fs.copyFile('./node_modules/jquery/dist/jquery.min.js',`js/vendor/jquery-${version}.min.js`,()=>undefined);
